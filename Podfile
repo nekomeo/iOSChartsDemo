198f86e9c60b7d8cebb72aaff3f22511f17085f0
nekomeo <linh.tu545@gmail.com>
2017-09-04 19:21:18 -0700

    Add chart data

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iOSChartsDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iOSChartsDemo
+
+  target 'iOSChartsDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  pod 'Charts'
+
+end
--- iOSChartsDemo/BarChartViewController.swift
+++ iOSChartsDemo/BarChartViewController.swift
@@ -13,11 +13,41 @@ class BarChartViewController: UIViewController {
 
     @IBOutlet weak var barChartView: BarChartView!
     
+    var months = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        barChartView.noDataTextDescription = "GIVE REASON"
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(values: unitsSold)
     }
-
+    
+    func setChart(values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart"
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<values.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [values[i]], data: months[i] as AnyObject)
+            //let dataEntry = BarChartDataEntry(x: Double(i + 1), y: values[i], data: months[i] as AnyObject)
+            //= BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntry.icon = #imageLiteral(resourceName: "icon")
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
+        //= BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        
+        barChartView.xAxis.labelPosition = .bottom
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:months) // Converts the months string to format that can be read on the graph
+        barChartView.xAxis.granularity = 1
+    
+        //= BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
+    }
 
 }
 
