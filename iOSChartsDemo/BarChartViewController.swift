7ff00a91a86068afe2677f9841ae5f7b61729fb4
nekomeo <linh.tu545@gmail.com>
2017-09-04 17:36:46 -0700

    Initial commit

---
+++ iOSChartsDemo/BarChartViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BarChartViewController.swift
+//  iOSChartsDemo
+//
+//  Created by Joyce Echessa on 6/12/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
